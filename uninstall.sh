79d905bbb475509d4115bd2cfe272d7264dc0255
kayusboy2001 <kayusboy2001@gmail.com>
2024-09-30 17:19:10 +0100

    Add uninstallation script

--- uninstall.sh
+++ uninstall.sh
@@ -16,16 +16,22 @@ apt purge -y system-update-tool
 
 rm -f /etc/apt/sources.list.d/system-update-tool.list
 
+# Remove the script from /usr/local/bin
+rm -f /usr/local/bin/system-update.sh
+
 # Update package lists
 apt update
 
 # Clean up any unnecessary packages
 apt autoremove -y
 
-# Remove the add-repo.sh script if it exists
-rm -f ~/add-repo.sh
+# Remove the add-repo.sh script if it exists in the current directory
+if [ -f "./add-repo.sh" ]; then
+    rm -f ./add-repo.sh
+    echo "Removed add-repo.sh"
+fi
 
-echo "Uninstallation complete. The system update tool has been removed."
+# Remove this uninstall script
+rm -f "$0"
 
-# Remove this uninstallation script
-rm -- "$0"
+echo "Uninstallation complete. The system update tool and all associated files have been removed."
